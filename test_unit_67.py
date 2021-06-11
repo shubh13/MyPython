664952b8e45a2d6c1ec12474b09d1c249a33e619
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-06-11 14:00:18 +0530

    First Unit Test

--- test_unit_67.py
+++ test_unit_67.py
@@ -15,3 +15,13 @@ def sum67(numbers):
             flag = True
 
     return result_sum
+
+class Test(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(sum67([1, 2, 2, 6, 7]), 5)
+
+    def test_case_2(self):
+        self.assertEqual(sum67([1, 2, 2, 6, 6, 7, 7, 2]), 14)
+
+    def test_case_3(self):
+        self.assertEqual(sum67([1, 2, 2, 3, 6, 6, 7, 7, 2]), 17)
